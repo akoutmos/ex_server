60e1ca77f63c3eea69d03cccc209aa57d9885efd
Alex Koutmos <akoutmos@gmail.com>
2022-02-16 22:17:55 -0500

    Printing directory contents

--- lib/server_web/live/file_explorer_live.ex
+++ lib/server_web/live/file_explorer_live.ex
@@ -5,7 +5,39 @@ defmodule ServerWeb.FileExplorerLive do
 
   use ServerWeb, :live_view
 
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, :temperature, 10)}
+  def mount(%{"path" => requested_path}, _session, socket) do
+    assigns = [
+      entries: get_files_from_path(requested_path),
+      current_path: requested_path
+    ]
+
+    {:ok, assign(socket, assigns)}
+  end
+
+  defp get_files_from_path([]) do
+    get_files_from_path("/")
+  end
+
+  defp get_files_from_path(requested_path) when is_list(requested_path) do
+    requested_path
+    |> Path.join()
+    |> get_files_from_path()
+  end
+
+  defp get_files_from_path(requested_path) do
+    cwd =
+      File.cwd!()
+      |> Path.join(requested_path)
+
+    cwd
+    |> File.ls!()
+    |> Enum.map(fn entry ->
+      file_stat =
+        cwd
+        |> Path.join(entry)
+        |> File.stat!()
+
+      {entry, file_stat}
+    end)
   end
 end
